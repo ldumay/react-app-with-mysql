4c8ef350d91588e48d045d692cd67ce47658e222
Loic DUMAY <dumay.famille@gmail.com>
2022-05-16 10:13:51 +0200

    v0.0.5

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import TaskList from "./components/TaskList";
 
 function App() {
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,45 +1,78 @@
 import React from "react";
+import {Button, Card, Col, FormControl, InputGroup, Row} from "react-bootstrap";
 
 class TaskList extends React.Component{
 	state = {
+		variant: "info",
 		task:""
 	}
 
+	onDoneClick = () => {
+		console.log('inside done')
+	}
 	onDeleteClick = () => {
 		console.log('inside delete')
+	}
+	onSubmitClick = () => {
+		console.log('inside onsubmit')
 	}
 
 	render(){
 		return(
 			<div>
-				<div>TaskList</div>
-				<div className="ui input">
-					<input
-						value={this.state.task}
-						onChange={e => this.setState({
-							task: e.target.value
-						})}
-						placeholder="your task ..."/>
-				</div>
-				<button className="ui primary button basic">Submit</button>
-				<hr/>
-				<div className="ui cards">
-					<div className="card">
-						<div className="content">
-							<div className="meta">
-								Friends of Veronika
-							</div>
-							<div className="extra content">
-								<div className="ui two buttons">
-									<div className="ui basic green button">Done</div>
-									<div className="ui basic red button" onClick={
-										() => this.onDeleteClick()
-									}>Delete</div>
-								</div>
-							</div>
-						</div>
+				<Row>
+					<div>
+						<h1>TaskList</h1>
 					</div>
-				</div>
+					<hr/>
+					<div>
+						<Row>
+							<Col></Col>
+							<Col>
+								<InputGroup className="mb-3">
+									<FormControl
+										placeholder="your task ..."
+										aria-label="your task ..."
+										aria-describedby="basic-addon2"
+										value={this.state.task}
+										onChange={e => this.setState({
+											task: e.target.value
+										})}
+									/>
+									<Button variant="outline-success" id="button-addon2" onClick={
+										() => this.onSubmitClick()
+									}>Submit</Button>
+								</InputGroup>
+							</Col>
+							<Col></Col>
+						</Row>
+					</div>
+					<hr/>
+					<Col>
+						<Card
+							bg={this.state.variant.toLowerCase()}
+							key={this.state.variant}
+							text={this.state.variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+							style={{ width: '18rem' }}
+							className="mb-2"
+						>
+							<Card.Header>Header</Card.Header>
+							<Card.Body>
+								<Card.Title>Primary Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make up the bulk
+									of the card's content.
+								</Card.Text>
+								<Button variant="success" onClick={
+									() => this.onDoneClick()
+								}>Done</Button>
+								<Button variant="danger" onClick={
+									() => this.onDeleteClick()
+								}>Delete</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
 			</div>
 		)
 	}
